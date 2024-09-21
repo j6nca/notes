882bdc0ec45a8e48ecb6c56d1f7120f288e7ec50
jonathan ng <me@j6n.ca>
2024-09-21 01:34:53 -0400

    update quartz cfgs

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 My digital garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "notes.j6n.ca",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
